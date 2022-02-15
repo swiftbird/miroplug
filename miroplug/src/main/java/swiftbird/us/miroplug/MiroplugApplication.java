de8df542babb12aa27aafe1124d6bd6f76618472
Shaun Anderson <shaun@vmware.com>
2022-02-15 16:19:27 -0700

    Initial

---
+++ miroplug/src/main/java/swiftbird/us/miroplug/MiroplugApplication.java
@@ -0,0 +1,13 @@
+package swiftbird.us.miroplug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiroplugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiroplugApplication.class, args);
+	}
+
+}
