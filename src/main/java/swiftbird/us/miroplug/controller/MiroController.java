376d45b2fc0f0ca86bd584077a3c449f2fbc5c8b
Shaun Anderson <shaun@vmware.com>
2022-02-16 12:45:41 -0700

    Initial

--- src/main/java/swiftbird/us/miroplug/controller/MiroController.java
+++ src/main/java/swiftbird/us/miroplug/controller/MiroController.java
@@ -1,4 +1,19 @@
 package swiftbird.us.miroplug.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value ="/")
+@RestController
 public class MiroController {
+
+    @GetMapping(value = "/graphs")
+    @ResponseBody
+    public String getAll() {
+        return "You did it";
+
+    }
+
 }
